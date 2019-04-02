48237b9afda04870ef34bfd40fcca37cffcc6b62
Andrea Silvi <andrea.silvi@silicondev.it>
2019-04-02 14:44:45 +0200

    last little endian fix was not good, shold be better

--- customtype.cpp
+++ customtype.cpp
@@ -17,44 +17,42 @@ std::string ByteType::getHexString()
     constexpr char hexmap[] = {'0', '1', '2', '3', '4', '5', '6', '7',
                        '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
     std::string s(this->size() * 2, ' ');
-    for (int i = 0; i < this->size(); ++i) {
+    for (size_t i = 0; i < this->size(); ++i) {
     s[2 * i]     = hexmap[(this->bytes[i] & 0xF0) >> 4];
     s[2 * i + 1] = hexmap[this->bytes[i] & 0x0F];
     }
     return s;
 }
-
 template <class T>
 T CustomType<T>::get()
 {
     T value;
     byte* buffer = (byte*)&value;
-	
-	if(!this->littleEndian)
-    {
-        std::reverse(this->bytes.begin(), this->bytes.end());
-    }
-	
-    for(size_t i=0 ; i<this->bytes.size() ; i++)
-        buffer[i] = this->bytes[i];
+
+	for(size_t i=0 ; i<this->bytes.size() ; i++)
+        if(!this->littleEndian)
+			buffer[i] = this->bytes[i];
+        else
+			buffer[i] = this->bytes[this->bytes.size() - i - 1];
 
     return value;
 }
+
 template <class T>
 void CustomType<T>::set(T value)
 {
     byte* buffer = (byte*)&value;
+
     for(size_t i=0 ; i<this->bytes.size() ; i++)
-        this->bytes[i] = buffer[i];
-	if(!this->littleEndian)
-    {
-        std::reverse(this->bytes.begin(), this->bytes.end());
-    }
+        if(!this->littleEndian)
+            this->bytes[i] = buffer[i];
+        else
+            this->bytes[i] = buffer[this->bytes.size() - i - 1];
 }
 
 Int24::Int24(bool littleEndian)
 {
-    this->bytes = std::vector<byte>(3);
+    this->bytes = std::vector<byte>(3,0);
     this->littleEndian = littleEndian;
 }
 Int24::Int24(int value, bool littleEndian) : Int24(littleEndian)
@@ -64,7 +62,7 @@ Int24::Int24(int value, bool littleEndian) : Int24(littleEndian)
 
 Int32::Int32(bool littleEndian)
 {
-    this->bytes = std::vector<byte>(4);
+    this->bytes = std::vector<byte>(4,0);
     this->littleEndian = littleEndian;
 }
 Int32::Int32(int value, bool littleEndian) : Int32(littleEndian)
@@ -74,7 +72,7 @@ Int32::Int32(int value, bool littleEndian) : Int32(littleEndian)
 
 Int64::Int64(bool littleEndian)
 {
-    this->bytes = std::vector<byte>(8);
+    this->bytes = std::vector<byte>(8,0);
     this->littleEndian = littleEndian;
 }
 Int64::Int64(long long int value, bool littleEndian) : Int64(littleEndian)
@@ -84,7 +82,7 @@ Int64::Int64(long long int value, bool littleEndian) : Int64(littleEndian)
 
 Float32::Float32(bool littleEndian)
 {
-    this->bytes = std::vector<byte>(4);
+    this->bytes = std::vector<byte>(4,0);
     this->littleEndian = littleEndian;
 }
 Float32::Float32(float value, bool littleEndian) : Float32(littleEndian)
@@ -94,7 +92,7 @@ Float32::Float32(float value, bool littleEndian) : Float32(littleEndian)
 
 Float64::Float64(bool littleEndian)
 {
-    this->bytes = std::vector<byte>(8);
+    this->bytes = std::vector<byte>(8,0);
     this->littleEndian = littleEndian;
 }
 Float64::Float64(double value, bool littleEndian) : Float64(littleEndian)
@@ -111,21 +109,37 @@ Octetstring::Octetstring(std::string value, bool littleEndian) : Octetstring(lit
 {
     this->set(value);
 }
+
 void Octetstring::set(std::string value)
 {
-    this->bytes.clear();
-    for(char i : value)
-    {
-        this->bytes.insert(this->bytes.begin(), (byte)i);
-    }
+    this->bytes = std::vector<byte>(value.size(),0);
 
-    if(!this->littleEndian)
-    {
-        std::reverse(this->bytes.begin(), this->bytes.end());
-    }
+    for(size_t i=0 ; i<this->size() ; i++)
+        if(!this->littleEndian)
+			this->bytes[i] = (byte) value.at(i);
+        else
+			this->bytes[i] = (byte) value.at(this->bytes.size() - i - 1);
 }
 std::string Octetstring::get()
 {
-    std::string newString((char*) &(this->bytes[0]));
+    char tempArr[this->size()];
+
+    for(size_t i=0 ; i<this->bytes.size() ; i++)
+        if(!this->littleEndian)
+			tempArr[i] = (char) this->bytes[i];
+        else
+			tempArr[i] = (char) this->bytes[this->bytes.size() - i - 1];
+    /*if(!this->littleEndian)
+    {
+        for(size_t i=this->bytes.size()-1 ; i==0 ; i--)
+			tempArr[i] = (char) this->bytes[i];
+    }
+	else
+	{
+		for(size_t i=0 ; i<this->bytes.size() ; i++)
+			tempArr[i] = (char) this->bytes[i];
+	}*/
+
+    std::string newString(tempArr);
     return newString;
 }
